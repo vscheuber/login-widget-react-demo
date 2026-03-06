56a2168a2ccc4fd0e29653ea1a3964fbbf82daf6
Volker Scheuber <vscheuber@gmail.com>
2026-03-06 15:33:32 -0700

    Logout button and userinfo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./src/certs/scheuber.io.key'),
+      cert: fs.readFileSync('./src/certs/scheuber.io.cer'),
+      ca: fs.readFileSync('./src/certs/alphassl_intermediate_gsgccr6alphasslca2025.crt'),
+    },
+    allowedHosts: ['localhost', 'login.scheuber.io'],
+  },
 })
